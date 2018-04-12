940d53e347e9458d67483453e04af22dfa8987fe
Tudor <tudoramariei@gmail.com>
2018-04-12 18:46:42 +0300

    Added functionality to plot the power spectrum and more easily pass in a file

--- graph_eeg.py
+++ graph_eeg.py
@@ -1,54 +1,73 @@
+from scipy import signal
+
 import csv
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 
+def get_pandas_data_set(selected_data_set):
+    """
+    Passes in the index of the data set we want to use
+    The list is statically built based on the files we have in ./resurse/
+    """
+
+    csv_sets = [
+        "Tibi1_rest1_[2017.06.14-11.25.26].csv",
+        "tibi1_rest1_cpy.csv",
+        "TUI04_2back_[2017.06.14-11.11.18].csv",
+        "TUI4_VTE_1Random_[2017.06.14-11.15.03].csv",
+        "TUI4_VTE_Boxes_[2017.06.14-11.20.56].csv",
+        "TUI4_VTE_Complex_[2017.06.14-11.22.59].csv",
+        "TUI4_VTE_Rest_[2017.06.14-11.07.20].csv",
+        "TUI5_3Back-[2017.05.24-15.48.50].csv",
+        "TUI5_3Back-modded.csv"
+    ]
+
+    csv_file = (
+        "resurse/"
+        "{0}".format(csv_sets[selected_data_set])
+    )
+
+    df = pd.read_csv(
+        csv_file,
+        delimiter=';'
+    )
+
+    return df
+
+
+def plot_power_spectrum_periodogram(data):
+    """
+    Create a power spectrum using the periodogram method
+    When plotting, we are limiting the printed frequency to 256 for clarity
+    The selected frequency is half of the sampling rate
+    """
+
+    f, pxx = signal.periodogram(
+        data,
+        fs=512.0,
+        window='flattop',
+        scaling='spectrum'
+        )
+
+    plt.semilogy(f, pxx)
+    plt.xlim(0, 256)
+    plt.show()
+
+
 def plot_data(x_data, y_data, color):
+    """
+    A simple method to quickly plot basic data
+    It helps with not forgetting to plot() and show()
+    """
+
     plt.plot(x_data, y_data, color)
     plt.show()
 
 
-def get_headers(df):
-    return df.column.values
+data_set = get_pandas_data_set(3)
 
+FP1_data = data_set["FP1"]
 
-def get_elements_from_rows(df, row_start, row_stop):
-    return df.iloc[row_start:row_stop]
-
-
-def get_elements_for_header_from_rows(df, header, row_start, row_stop):
-    return df[header][row_start:row_stop]
-
-
-csv_headers = [
-    "Time", "FP1", "F7", "F3", "C3", "P3", "P7",
-    "O1", "O2", "FP2", "F8", "F4", "C4", "P4", "P8",
-    "Pz", "T8", "Sampling Rate"
-]
-
-csv_file = """\
-resurse/\
-TUI5_3Back-modded.csv\
-"""
-
-df = pd.read_csv(
-    csv_file,
-    delimiter=';'
-    )
-
-x = "Time"
-y = "FP1"
-z = "F7"
-
-x_data = df["Time"]
-y_data = df["FP1"]
-z_data = df["F7"]
-
-# Plotting all the electrodes, each in separate subplots
-df.head()
-df.plot(
-    x=x,
-    subplots=True,
-    legend=False
-)
-plt.show()
+plot_power_spectrum_periodogram(FP1_data)
