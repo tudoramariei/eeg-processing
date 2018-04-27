035d3dbdb9e13294bfd6ec9187895518f5e14700
Tudor <tudoramariei@gmail.com>
2018-04-27 23:39:03 +0300

    Added functionality for butterworth bandpass filtering Added relevant comments to all of the functions Removed useless imports Clean-up

--- graph_eeg.py
+++ graph_eeg.py
@@ -1,9 +1,23 @@
 from scipy import signal
 
-import csv
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+
+
+def get_headers(df):
+    """
+    Passes a dataframe and returns the headers for it
+    """
+    return df.columns.values
+
+
+def data_normalization(dataframe):
+    """
+    Bring the dataframe values in the range [0, 1]
+    Needed in order for the date to work in a butterworth filter
+    """
+
+    return dataframe.apply(lambda x: (x - x.min()) / (x.max() - x.min()))
 
 
 def get_pandas_data_set(selected_data_set):
@@ -14,14 +28,12 @@ def get_pandas_data_set(selected_data_set):
 
     csv_sets = [
         "Tibi1_rest1_[2017.06.14-11.25.26].csv",
-        "tibi1_rest1_cpy.csv",
         "TUI04_2back_[2017.06.14-11.11.18].csv",
         "TUI4_VTE_1Random_[2017.06.14-11.15.03].csv",
         "TUI4_VTE_Boxes_[2017.06.14-11.20.56].csv",
         "TUI4_VTE_Complex_[2017.06.14-11.22.59].csv",
         "TUI4_VTE_Rest_[2017.06.14-11.07.20].csv",
-        "TUI5_3Back-[2017.05.24-15.48.50].csv",
-        "TUI5_3Back-modded.csv"
+        "TUI5_3Back-[2017.05.24-15.48.50].csv"
     ]
 
     csv_file = (
@@ -34,7 +46,19 @@ def get_pandas_data_set(selected_data_set):
         delimiter=';'
     )
 
-    return df
+    time = df['Time (s)']
+
+    df = df.drop(columns=[
+        'Time (s)',
+        'Sampling Rate'
+    ])
+
+    if 'Reference' in df.columns:
+        df = df.drop(columns=[
+            'Reference'
+        ])
+
+    return data_normalization(df), time
 
 
 def plot_power_spectrum_periodogram(data):
@@ -44,9 +68,10 @@ def plot_power_spectrum_periodogram(data):
     The selected frequency is half of the sampling rate
     """
 
+    _sampling_rate = 512.0
     f, pxx = signal.periodogram(
         data,
-        fs=512.0,
+        fs=_sampling_rate,
         window='flattop',
         scaling='spectrum'
         )
@@ -55,7 +80,7 @@ def plot_power_spectrum_periodogram(data):
     plt.show()
 
 
-def plot_data(x_data, y_data, color):
+def plot_data(x_data, y_data, color='r'):
     """
     A simple method to quickly plot basic data
     It helps with not forgetting to plot() and show()
@@ -65,7 +90,63 @@ def plot_data(x_data, y_data, color):
     plt.show()
 
 
-data_set = get_pandas_data_set(3)
-FP1_data = data_set["FP1"]
+def butter_bandpass(
+        lowcut_freq,
+        highcut_freq,
+        sampling_rate,
+        order
+        ):
+    """
+    Return the desired butterworth bandpass filter
+    """
 
-plot_power_spectrum_periodogram(FP1_data)
+    _nyq = 0.5 * sampling_rate
+    _low = lowcut_freq/_nyq
+    _high = highcut_freq/_nyq
+
+    a, b = signal.butter(
+        N=order,
+        Wn=[_low, _high],
+        btype='bandpass'
+        )
+
+    return a, b
+
+
+def butter_bandpass_filter(data_vector):
+    """
+    Filters the signal with the designated butterworth bandpass filter
+    """
+
+    _lowcut_freq = 0.5
+    _highcut_freq = 5.0
+    _sampling_rate = 512
+    _order = 4
+
+    a, b = butter_bandpass(
+        _lowcut_freq,
+        _highcut_freq,
+        _sampling_rate,
+        _order
+        )
+    y = signal.lfilter(a, b, data_vector)
+
+    return y
+
+
+def get_butter(df):
+    """
+    Filters the dataframe with a butterworth bandpass filter
+    """
+
+    for column in df.columns.values:
+        df_column = df[column]
+        df[column] = butter_bandpass_filter(df_column)
+
+    return df
+
+
+df, time_df = get_pandas_data_set(2)
+df = get_butter(df)
+
+plt.show()
